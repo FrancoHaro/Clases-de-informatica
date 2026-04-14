80bfa233fbe590ab9955d70c00fa76e2a3f32097
Franco Haro <e21135@ism.edu.ec>
2026-04-14 10:20:52 -0500

    Actividad 1

---
+++ Actividad_1.py
@@ -0,0 +1,26 @@
+"""Franco Haro
+Diploma 1 D
+9/4/2026"""
+print("Operaciones Matemáticas")
+print("Adición (+):", 3 + 4)
+print("Sustracción (-):", 3 - 4)
+print("Multiplicación (*):", 3 * 4)
+print("Mod (%):", 3 % 4)
+print("División (/):", 3 / 4)
+print("Exponenciación (**):", 3 ** 4)
+print("Div (//):", 3 // 4)
+
+print("Cadenas de Texto")
+print("Tu nombre")
+print("Tu apellido")
+print("Tu país")
+print("Estoy disfrutando de 30 días de Python")
+print("\n")
+
+print("Tipos de Datos")
+print("Tipo de 10:", type(10))
+print("Tipo de 9.8:", type(9.8))
+print("Tipo de 3.14:", type(3.14))
+print("Tipo de lista:", type(['Asabeneh', 'Python', 'Finland']))
+print("Tipo de 'Tu apellido':", type('Tu apellido'))
+print("Tipo de 'Tu país':", type('Tu país'))
