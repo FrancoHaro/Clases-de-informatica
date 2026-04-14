944647413af44189b36d5e5578067041d6c708d8
Franco Haro <e21135@ism.edu.ec>
2026-04-14 12:37:21 -0500

    primera actualizacion actividad 2

---
+++ Actividad 2 (variables).py
@@ -0,0 +1,13 @@
+print(len("Franco Haro"))
+first_name = "Franco"
+print("first name length",len(first_name))
+# %%
+first_name = "Franco"
+longitud = len(first_name)
+print(type(longitud))
+print(type(first_name))
+# %%
+# %%
+primer_nombre,apellido, pais, edad, estado_civil, = "Franco", "Haro", "Ecuador", 16, "Soltero"
+print("Primer nombre: ", primer_nombre, "Apellido: ", apellido)
+# %%
