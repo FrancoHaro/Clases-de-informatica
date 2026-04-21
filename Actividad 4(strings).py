fe8fbc712ef317a84b21174fb11057a0428a940f
Franco Haro <e21135@ism.edu.ec>
2026-04-21 10:57:21 -0500

    cambio1 actv3

---
+++ Actividad 4(strings).py
@@ -0,0 +1,19 @@
+stringVariasLineas = """
+Franco
+Haro
+Informatica
+"""
+print(stringVariasLineas)
+colegio = "iSM"
+longitud = len(colegio)
+print("La longitud de la palabra colegio es: ", longitud)
+print(len("ISM"))
+nombre = "Franco"
+apellido = "Haro "
+nombre_completo = nombre + " " + apellido
+print("Mi nombre completo es: ", nombre_completo)
+print(nombre_completo * 2)
+print("Phyton \nchallenge")
+print("Semana 1 \tSemana 2 \tSemana 3")
+print("Simbolo(\\)")
+print(f"Mi nombre es {nombre} y mi apellido es {apellido}")
